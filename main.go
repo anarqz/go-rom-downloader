cd1d5edf04b6b6429e85366f7f84eeec395d9bb1
Alexandre Moraes <alcmoraes89@gmail.com>
2018-06-27 20:08:05 -0300

    Fixing input scan method

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/url"
+	"os"
 
 	"github.com/alcmoraes/go-cr-scraper/sources"
+	"github.com/alcmoraes/go-cr-scraper/utils"
 )
 
 func main() {
 	source := sources.LoadSource("Coolrom", nil)
 
-	var gameToLookAt string
+	var holdTmp string
+	var outputFile string
+
 	fmt.Println("Enter the game you would like to search:")
-	fmt.Scanf("%s", &gameToLookAt)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	roms := source.Lookup(url.QueryEscape(gameToLookAt))
+	for scanner.Scan() {
+		gameToLookAt := scanner.Text()
 
-	for _, rom := range roms {
-		fmt.Printf("%s (%s): %s\n", rom.Name, rom.Console, rom.URL)
+		outputFile = utils.Slugify(gameToLookAt, true) + ".json"
+
+		roms := source.Lookup(url.QueryEscape(gameToLookAt))
+
+		if output, err := json.MarshalIndent(roms, "", "\t"); err == nil {
+			if err := ioutil.WriteFile(outputFile, output, 0644); err == nil {
+				fmt.Println("Download links stored in " + outputFile + " file. You can close this window now")
+				fmt.Scanf("%s", &holdTmp)
+			}
+		}
+
 	}
+
+	os.Exit(1)
+
 }
